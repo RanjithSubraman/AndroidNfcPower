bed646867892bda7ae992acfdbdaae0bc85d0d4d
Ranjith Subramaniam <ranjith.subraman@gmail.com>
2017-04-06 11:01:48 -0500

    Added a test for NfcController.

--- app/src/test/java/com/goovis/androidnfcpower/controller/NfcControllerTest.java
+++ app/src/test/java/com/goovis/androidnfcpower/controller/NfcControllerTest.java
@@ -1,12 +1,16 @@
 package com.goovis.androidnfcpower.controller;
 
 import android.content.Context;
+import android.nfc.NfcAdapter;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertFalse;
 
 /**
  * Created by ranjithsubramaniam on 4/6/17.
@@ -17,9 +21,19 @@ public class NfcControllerTest {
     private NfcController nfcController;
 
     @Mock Context mockContext;
+    @Mock NfcAdapter mockNfcAdapter;
 
     @Before
     public void initMocks() {
         MockitoAnnotations.initMocks(this);
+
+        nfcController = new NfcController(mockContext);
+    }
+
+    @Test
+    public void testCheckNfcPowerStatus() {
+        // We can't mock static class by using mockito, so we need power mockito.
+        // doNothing().when(NfcAdapter.getDefaultAdapter(mockContext));
+        assertFalse(nfcController.checkNfcPowerStatus(mockContext));
     }
 }
